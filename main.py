7939c0ed370400cbfaefecb9eab3d668564985cb
ortjk <ortjkdev@gmail.com>
2021-07-19 11:42:53 -0230

    added some directions to the robot in main.py, and updated robot.py according to a new patch given by roy

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ import matplotlib
 r = robot.RobotController()
 r.connect()
 
+# get to the first marker from spawn
+r.left(125)
+r.forward(350)
 
+# go left into room at marker
+if r.read_marker() == 1:
+    r.left(1000)
