74059edfaff4f1554d71b39bd5a969eb60c00cc1
ortjk <ortjkdev@gmail.com>
2021-07-20 11:22:20 -0230

    completed a working directions program for the robot

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import robot
 from matplotlib import pyplot as plt
 
-xs = []
-ys = []
-room_num = 0
-
 r = robot.RobotController()
 r.connect()
-
-xs.append(room_num)
-ys.append(r.take_temperature())
-room_num += 1
 
 r.left(125)
 r.forward(350)
@@ -18,21 +10,16 @@ r.forward(350)
 # go left into room at marker and rescue person
 if r.read_marker() == 1:
     r.left(640)
-    r.forward(50)
+    r.forward(0)
 
-    if r.scan_for_people():
-        r.rescue_person()
+    # add person rescue here
 
-    xs.append(room_num)
-    ys.append(r.take_temperature())
-    room_num += 1
-
-    r.backward(50)
+    r.backward(0)
     r.right(640)
 
     r.backward(350)
-    r.right(50)
-    r.left(50)
+    r.right(40)
+    r.left(40)
     r.forward(350)
 
 
@@ -41,16 +28,11 @@ r.forward(600)
 # go into room and extinguish fire
 if r.read_marker() == 1:
     r.left(600)
-    r.forward(75)
+    r.forward(30)
 
-    while r.scan_for_fire():
-        r.extinguish_fire()
+    # add fire extinguishing here
 
-    xs.append(room_num)
-    ys.append(r.take_temperature())
-    room_num += 1
-
-    r.backward(75)
+    r.backward(30)
     r.right(600)
 
 
@@ -62,33 +44,27 @@ r.forward(1600)
 if r.read_marker() == 1:
     r.left(75)
 
-    xs.append(room_num)
-    ys.append(r.take_temperature())
-    room_num += 1
+    # add temperature taking here
 
     r.right(75)
 
 
-r.forward(550)
+r.forward(520)
 r.left(750)
 r.backward(750)
 r.rotate_counterclockwise(90)
 r.forward(800)
 
 # go into room and extinguish fire
-if r.read_marker() == 1:
-    r.right(50)
+# add marker check here
+if True:
+    r.right(80)
     r.forward(400)
-    r.left(600)
+    r.left(500)
 
-    while r.scan_for_fire():
-        r.extinguish_fire()
+    # add extinguishing here
 
-    xs.append(room_num)
-    ys.append(r.take_temperature())
-    room_num += 1
-
-    r.right(600)
+    r.right(500)
     r.backward(400)
     r.left(50)
 
@@ -98,17 +74,13 @@ r.forward(200)
 r.left(450)
 
 # go into room and rescue person
-if r.read_marker() == 1:
+# add marker check here
+if True:
     r.right(50)
     r.forward(300)
     r.left(350)
 
-    if r.scan_for_people():
-        r.rescue_person()
-
-    xs.append(room_num)
-    ys.append(r.take_temperature())
-    room_num += 1
+    # add rescue code here
 
     r.right(350)
     r.backward(300)
@@ -124,7 +96,4 @@ r.backward(900)
 r.left(2200)
 r.forward(1400)
 
-plt.plot(xs, ys, 'ro')
-plt.xlabel('Room Number')
-plt.ylabel('Room Temperature')
-plt.show()
+# plot the graph
